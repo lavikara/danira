ca798eb73bd711b640d1784d1da8efcb25769488
lavikara <karatemitayo@gmail.com>
2026-06-19 12:03:04 +0100

    add random password generation function to helper file

---
+++ src/utils/helpers.ts
@@ -0,0 +1,30 @@
+export const generateRandomString = (): string => {
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbers = "0123456789";
+  const allChars = lower + upper + numbers;
+
+  const hasLower = /[a-z]/;
+  const hasUpper = /[A-Z]/;
+  const hasNumber = /[0-9]/;
+
+  let result = "";
+
+  while (true) {
+    result = "";
+    for (let i = 0; i < 8; i++) {
+      const randomIndex = Math.floor(Math.random() * allChars.length);
+      result += allChars[randomIndex];
+    }
+
+    if (
+      hasLower.test(result) &&
+      hasUpper.test(result) &&
+      hasNumber.test(result)
+    ) {
+      break;
+    }
+  }
+
+  return result;
+};
