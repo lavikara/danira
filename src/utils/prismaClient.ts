9306974dbc7a2a49dcc84014ad7647d4ea6069a1
lavikara <karatemitayo@gmail.com>
2026-06-19 11:59:07 +0100

    rename file

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -5,6 +5,6 @@ import { PrismaClient } from "../generated/client.js";
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prismaClient = new PrismaClient({ adapter });
 
-export { prisma };
+export { prismaClient };
