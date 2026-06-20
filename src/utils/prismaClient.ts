3b801f7d0a4b21d89b81a26e0a6f0d03241ffce3
lavikara <karatemitayo@gmail.com>
2026-06-20 18:18:42 +0100

    update database env url

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/client.js";
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.POSTGRES_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 const prismaClient = new PrismaClient({ adapter });
