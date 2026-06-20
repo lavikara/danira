2e8c4bdf1a91578c4a4be060b20d5ced4c4b56d0
lavikara <karatemitayo@gmail.com>
2026-06-20 18:36:55 +0100

    create global error and success response

---
+++ src/utils/apiResponse.ts
@@ -0,0 +1,23 @@
+import { ApiResponse } from "../types/definitions.js";
+
+export class SuccessResponse<T> implements ApiResponse<T> {
+  readonly success = true;
+  readonly timestamp = new Date().toISOString();
+
+  constructor(
+    public readonly message: string,
+    public readonly data: T,
+  ) {}
+}
+
+export class ApiError extends Error {
+  readonly name = "ApiError";
+
+  constructor(
+    public readonly statusCode: number,
+    message: string,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
