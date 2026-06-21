bfa48a174409d892468f0bae3531b4d5b910783d
lavikara <karatemitayo@gmail.com>
2026-06-21 19:07:38 +0100

    create email service

---
+++ src/services/emailServices/emailService.ts
@@ -0,0 +1,51 @@
+import * as nodemailer from "nodemailer";
+import { SignupSchoolInput } from "../../middleware/zodvalidate/schema/school/schoolSchemas.js";
+import { Users } from "../../generated/browser.js";
+import { crateSchoolTemplate } from "./emailTemplates/createSchoolTemplates.js";
+import { forgotPasswordTemplate } from "./emailTemplates/forgotPasswordTemplate.js";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure: true,
+  auth: {
+    user: process.env.NODEMAILER_EMAIL_USER,
+    pass: process.env.NODEMAILER_EMAIL_PASS,
+  },
+});
+
+export const schoolCreatedMail = (
+  data: SignupSchoolInput,
+  urlData: { token: string },
+) => {
+  const createSchoolMailOptions = {
+    from: '"Danira Admin" <no-reply@danira.com>',
+    to: `${data.schoolData.email}`,
+    subject: "Your School Account is ready",
+    html: crateSchoolTemplate(data, urlData),
+  };
+  transporter.sendMail(createSchoolMailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+export const forgotPasswordMail = (data: Users, urlData: { token: string }) => {
+  const forgotPasswordMailOptions = {
+    from: '"Danira Admin" <no-reply@danira.com>',
+    to: `${data.email}`,
+    subject: "Reset Your Password",
+    html: forgotPasswordTemplate(data, urlData),
+  };
+  transporter.sendMail(forgotPasswordMailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    } else {
+      console.log(info);
+    }
+  });
+};
