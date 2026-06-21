2d47a2ff031199ffb4d485d570efdb0e9f1f3185
lavikara <karatemitayo@gmail.com>
2026-06-21 19:03:57 +0100

    create user schema

---
+++ src/middleware/zodvalidate/schema/user/userSchema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { Role, UserCondition, Gender } from "../../../../generated/enums.js";
+
+export const userDataSchema = z.object({
+  username: z.string().min(3, "Username must be at least 3 characters"),
+  email: z.email("Invalid admin email address"),
+  firstName: z.string().min(1, "First name is required"),
+  lastName: z.string().min(1, "Last name is required"),
+  phoneNumber: z.string(),
+  address: z.string(),
+  isVerified: z.boolean(),
+  role: z.enum(Role),
+  gender: z.enum(Gender),
+  status: z.enum(UserCondition),
+});
+
+export type UserDataInput = z.infer<typeof userDataSchema>;
