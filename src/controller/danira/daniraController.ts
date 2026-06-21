af36ad05f3332416cffa00106b5f1e30ad26ea3d
lavikara <karatemitayo@gmail.com>
2026-06-21 19:00:37 +0100

    add conditions to ensure proper role assignment from teh frontend

--- src/controller/danira/daniraController.ts
+++ src/controller/danira/daniraController.ts
@@ -1,12 +1,13 @@
 import { type Request, type Response, type NextFunction } from "express";
 import bcrypt from "bcryptjs";
-import { SignupSingleSchoolInput } from "../../middleware/zodvalidate/schema/school/schoolSchemas.js";
+import { SignupSchoolInput } from "../../middleware/zodvalidate/schema/school/schoolSchemas.js";
 import { schoolCreatedMail } from "../../services/emailServices/emailService.js";
 import { sign } from "../../services/jwtService/jwtService.js";
 import { generateRandomString } from "../../utils/helpers.js";
 import { SuccessResponse, ApiError } from "../../utils/apiResponse.js";
 import {
-  signupSchool,
+  signupSingleSchool,
+  signupGroupSchool,
   querySchoolTableByEmailAndSchoolName,
 } from "../../services/dbServices/schoolTable.js";
 
@@ -14,7 +15,7 @@ import {
  *    Single school signup logic for super admin
  */
 export const daniraSingleSchoolSignup = async (
-  req: Request<SignupSingleSchoolInput>,
+  req: Request<SignupSchoolInput>,
   res: Response,
   next: NextFunction,
 ) => {
@@ -23,12 +24,33 @@ export const daniraSingleSchoolSignup = async (
     return next(error);
   }
 
+  if (
+    req.body.schoolData.setup === "GROUP" &&
+    req.body.adminData.role !== "GROUPSCHOOLADMIN"
+  ) {
+    const error = new ApiError(
+      422,
+      "Role for group school admin must be GROUPSCHOOLADMIN.",
+    );
+    return next(error);
+  }
+
+  if (
+    req.body.schoolData.setup === "SINGLE" &&
+    req.body.adminData.role !== "SCHOOLADMIN"
+  ) {
+    const error = new ApiError(
+      422,
+      "Role for single school admin must be SCHOOLADMIN.",
+    );
+    return next(error);
+  }
+
   const randomPassword = generateRandomString();
   const defaultPassword = bcrypt.hashSync(randomPassword);
   req.body.adminData.password = defaultPassword;
   req.body.schoolData.createdBy = req.userId;
   req.body.schoolData.approvedBy = req.userId;
-
   const schoolAlreadyExist = await querySchoolTableByEmailAndSchoolName(
     req.body.schoolData,
   );
@@ -39,7 +61,10 @@ export const daniraSingleSchoolSignup = async (
   }
 
   if (!schoolAlreadyExist) {
-    const created = await signupSchool(req.body);
+    let created = null;
+    req.body.schoolData.setup === "SINGLE"
+      ? (created = await signupSingleSchool(req.body))
+      : (created = await signupGroupSchool(req.body));
     if (created) {
       const token = sign({
         admins: created.admin.id,
