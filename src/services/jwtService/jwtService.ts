5ed110f9fbbaf6bef6fc6250cf3f061e2d5ea137
lavikara <karatemitayo@gmail.com>
2026-06-24 01:19:47 +0100

    update sign and verify function  to Asynchronous functions

--- src/services/jwtService/jwtService.ts
+++ src/services/jwtService/jwtService.ts
@@ -1,26 +1,45 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { RelationKeysMap } from "../../types/definitions.js";
+import { logger } from "../../utils/logger.js";
 
 export const sign = (
   data: RelationKeysMap,
   expireIn: number = 86400,
-): string => {
-  const token = jwt.sign(data, process.env.JWT_SECRET as string, {
-    expiresIn: expireIn,
+): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    jwt.sign(
+      data,
+      process.env.JWT_SECRET as string,
+      { expiresIn: expireIn },
+      (error, token) => {
+        if (error) {
+          return logger.error({ message: error.message }, "JWT Error");
+        }
+        if (!token) {
+          return logger.error(
+            { message: "Token generation failed: Token is undefined" },
+            "JWT Error",
+          );
+        }
+        resolve(token);
+      },
+    );
   });
-  return token;
 };
 
 export const verify = (token: string | undefined): {} => {
-  const verified: any = jwt.verify(
-    token as string,
-    process.env.JWT_SECRET as string,
-    (err, decoded) => {
-      if (err) {
-        console.log(err);
-      }
-      if (decoded) return decoded;
-    },
-  );
-  return verified;
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token as string,
+      process.env.JWT_SECRET as string,
+      (error, decoded) => {
+        if (error) {
+          return logger.error({ message: error.message }, "JWT Error");
+        }
+
+        // Typecast the safely verified payload to your custom interface
+        resolve(decoded as any);
+      },
+    );
+  });
 };
