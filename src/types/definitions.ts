7fd09ac2c506e3d779a88dbc7a771c8e2a8ac492
lavikara <karatemitayo@gmail.com>
2026-06-24 04:51:14 +0100

    add relevant type definitions

--- src/types/definitions.ts
+++ src/types/definitions.ts
@@ -1,3 +1,19 @@
+import {
+  Users,
+  Guardians,
+  Admins,
+  Staffs,
+  Students,
+} from "../generated/browser.js";
+
+export interface UserToRelation {
+  users: Users & {
+    guardians?: Guardians;
+    admins?: Admins;
+    staffs?: Staffs;
+    students?: Students;
+  };
+}
 export interface ApiResponse<T = any> {
   success: boolean;
   message: string;
@@ -5,16 +21,32 @@ export interface ApiResponse<T = any> {
   timestamp: string;
 }
 
-export type RelationKeys = "admins" | "students" | "staffs" | "guardians";
+export type RelationKeys =
+  | "admins"
+  | "students"
+  | "staffs"
+  | "guardians"
+  | "users";
 
 export type RelationKeysMap = {
   [K in RelationKeys]?: string;
 };
 
-export interface UserQueryOptions {
-  omitPassword?: boolean;
+export type IncludeQuery = {
   admins?: boolean;
   students?: boolean;
   staffs?: boolean;
   guardians?: boolean;
-}
+  users?: boolean;
+};
+
+export type OmitQuery = {
+  [K in
+    | keyof Users
+    | keyof Admins
+    | keyof Staffs
+    | keyof Guardians
+    | keyof Students]?: boolean;
+};
+
+export type TableColumn = "email" | "id";
