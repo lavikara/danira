03be4cb30e8d1bf6dea7569ff315a0e0e6239cd3
lavikara <karatemitayo@gmail.com>
2026-06-28 19:59:36 +0100

    add approve school route

--- src/routes/danira/daniraRouter.ts
+++ src/routes/danira/daniraRouter.ts
@@ -1,14 +1,16 @@
-import { Router } from "express";
-import { catchAsync } from "../../utils/catchAsync.js";
-import { validate } from "../../middleware/zodvalidate/validate.js";
-import { signupSchoolSchema } from "../../middleware/zodvalidate/schema/school/schoolSchemas.js";
-import { authJwtAndRole } from "../../middleware/authorization/authorization.js";
-import { Role } from "../../generated/browser.js";
+import { Router } from 'express';
+import { catchAsync } from '../../utils/catchAsync.js';
+import { validate } from '../../middleware/zodvalidate/validate.js';
+import { authJwtAndRole } from '../../middleware/authorization/authorization.js';
+import { Role } from '../../generated/browser.js';
 import {
-  invalidateSchool,
+  signupSchoolSchema,
+  approveOrInvalidateSchoolSchema,
+} from '../../middleware/zodvalidate/schema/school/schoolSchemas.js';
+import {
   approveSchool,
   daniraSingleSchoolSignup,
-} from "../../controller/danira/daniraController.js";
+} from '../../controller/danira/daniraController.js';
 
 const router = Router();
 
@@ -16,14 +18,17 @@ const router = Router();
  *    This route handles school signup by a super admin
  */
 router.post(
-  "/signup-single-school",
+  '/signup-school',
   authJwtAndRole([Role.SUPERADMIN]),
   validate({ body: signupSchoolSchema }),
   catchAsync(daniraSingleSchoolSignup),
 );
 
-router.post("/approve-school", approveSchool);
-
-router.post("/invalidate-school", invalidateSchool);
+router.post(
+  '/approve-school',
+  authJwtAndRole([Role.SUPERADMIN]),
+  validate({ body: approveOrInvalidateSchoolSchema }),
+  approveSchool,
+);
 
 export default router;
