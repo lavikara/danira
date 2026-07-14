1fbbcfbe4bee250fcc73a96ec2534965c8cee6bc
lavikara <karatemitayo@gmail.com>
2026-07-14 17:26:07 +0100

    refactor code

--- src/services/schoolService/singleSchoolService.ts
+++ src/services/schoolService/singleSchoolService.ts
@@ -1,12 +1,8 @@
 import { findUniqueSchool } from '../dbServices/dbServices.js';
 import { ReturnResponse } from '../../types/definitions.js';
 
-export const schoolDetails = async (schoolIds: string[]): Promise<ReturnResponse> => {
-  const id = schoolIds[0];
-  const schools = await findUniqueSchool('schools', 'id', id as string, {
-    staffs: true,
-    students: true,
-  });
+export const schoolDetails = async (schoolId: string): Promise<ReturnResponse> => {
+  const schools = await findUniqueSchool('schools', 'id', schoolId);
   if (schools) {
     const response = { success: true, message: 'School details fetched', data: schools };
     return response;
