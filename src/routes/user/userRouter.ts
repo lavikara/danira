13e5b77384c3606ca5f87fd2a8b64b9d231ca5cf
lavikara <karatemitayo@gmail.com>
2026-07-14 17:21:57 +0100

    refactor user route  to use user and role authorisation

--- src/routes/user/userRouter.ts
+++ src/routes/user/userRouter.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { catchAsync } from '../../utils/catchAsync.js';
-import { authJwtAndRole } from '../../middleware/authorization/authorization.js';
+import { roleAuthorization } from '../../middleware/authorization/roleAuthorization.js';
+import { userAuthorization } from '../../middleware/authorization/userAuthorization.js';
 import { Role } from '../../generated/browser.js';
 import { loggedInUser } from '../../controller/user/userController.js';
 
 const router = Router();
 
-router.get('/me', authJwtAndRole(Object.values(Role) as Role[]), catchAsync(loggedInUser));
+router.get(
+  '/me',
+  userAuthorization,
+  roleAuthorization(Object.values(Role) as Role[]),
+  catchAsync(loggedInUser),
+);
 
 export default router;
