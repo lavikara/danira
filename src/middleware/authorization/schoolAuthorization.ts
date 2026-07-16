c29800d314ef4e6f1ffeff1b9d5b9995a20ae4e2
lavikara <karatemitayo@gmail.com>
2026-07-16 04:07:03 +0100

    implement group authorization

--- src/middleware/authorization/schoolAuthorization.ts
+++ src/middleware/authorization/schoolAuthorization.ts
@@ -1,1 +1,14 @@
-export const schoolAuthorization = () => {};
+import { Request, Response, NextFunction } from 'express';
+import { ApiError } from '../../utils/apiResponse.js';
+
+export const schoolAuthorization = (req: Request, res: Response, next: NextFunction) => {
+  const schoolId = req.params.schoolId;
+  const schoolIds = req.schoolIds as string[];
+  const isAuthorized = schoolId ? schoolIds.includes(schoolId as string) : false;
+  if (!isAuthorized) {
+    const error = new ApiError(401, "You don't have access to this resource");
+    return next(error);
+  }
+
+  return next();
+};
