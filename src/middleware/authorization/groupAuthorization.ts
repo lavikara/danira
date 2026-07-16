cc653e7addb8e42807e435c6bee68f827cc994e0
lavikara <karatemitayo@gmail.com>
2026-07-16 04:06:10 +0100

    implement group authorization

---
+++ src/middleware/authorization/groupAuthorization.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { ApiError } from '../../utils/apiResponse.js';
+
+export const groupAuthorization = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req);
+  console.log(req.params.groupId);
+  const groupId = req.params.groupId;
+  const isAuthorized = groupId ? groupId === req.groupId : false;
+  if (!isAuthorized) {
+    const error = new ApiError(401, "You don't have access to this resource");
+    return next(error);
+  }
+
+  return next();
+};
