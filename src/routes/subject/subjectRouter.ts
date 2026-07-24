a77de3ccbc71085f394293c2965c8f2bd899518a
lavikara <karatemitayo@gmail.com>
2026-07-24 17:51:43 +0100

    implement subject router

---
+++ src/routes/subject/subjectRouter.ts
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import { catchAsync } from '../../utils/catchAsync.js';
+import { roleAuthorization } from '../../middleware/authorization/roleAuthorization.js';
+import { userAuthorization } from '../../middleware/authorization/userAuthorization.js';
+import { schoolAuthorization } from '../../middleware/authorization/schoolAuthorization.js';
+import { groupAuthorization } from '../../middleware/authorization/groupAuthorization.js';
+import { pagination } from '../../middleware/pagination/pagination.js';
+import { Role } from '../../generated/browser.js';
+import {
+  allSingleSchoolSubject,
+  singleSchoolSubjectAnalytics,
+  groupSubjectAnalytics,
+  allGroupSchoolSubject,
+} from '../../controller/subject/subjectController.js';
+
+const router = Router();
+
+router.get(
+  '/:schoolId/all',
+  userAuthorization,
+  roleAuthorization([Role.SCHOOLADMIN, Role.SUBSCHOOLADMIN, Role.GROUPSCHOOLADMIN]),
+  schoolAuthorization,
+  pagination,
+  catchAsync(allSingleSchoolSubject),
+);
+
+router.get(
+  '/:schoolId/analytics',
+  userAuthorization,
+  roleAuthorization([Role.SCHOOLADMIN, Role.SUBSCHOOLADMIN, Role.GROUPSCHOOLADMIN]),
+  schoolAuthorization,
+  catchAsync(singleSchoolSubjectAnalytics),
+);
+
+router.get(
+  '/:groupId/all-group',
+  userAuthorization,
+  roleAuthorization([Role.GROUPSCHOOLADMIN]),
+  groupAuthorization,
+  pagination,
+  catchAsync(allGroupSchoolSubject),
+);
+
+router.get(
+  '/:groupId/analytics-group',
+  userAuthorization,
+  roleAuthorization([Role.GROUPSCHOOLADMIN]),
+  groupAuthorization,
+  catchAsync(groupSubjectAnalytics),
+);
+
+export default router;
