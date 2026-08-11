df3afcbe42b451ff7f241931bb426b311aeefb53
lavikara <karatemitayo@gmail.com>
2026-08-11 01:43:27 +0100

    add school staff route

--- src/routes/student/studentRouter.ts
+++ src/routes/student/studentRouter.ts
@@ -2,15 +2,18 @@ import { Router } from 'express';
 import { catchAsync } from '../../utils/catchAsync.js';
 import { roleAuthorization } from '../../middleware/authorization/roleAuthorization.js';
 import { userAuthorization } from '../../middleware/authorization/userAuthorization.js';
+import { staffAuthorization } from '../../middleware/authorization/staffAuthorization.js';
 import { schoolAuthorization } from '../../middleware/authorization/schoolAuthorization.js';
 import { groupAuthorization } from '../../middleware/authorization/groupAuthorization.js';
 import { pagination } from '../../middleware/pagination/pagination.js';
 import { Role } from '../../generated/browser.js';
 import {
   allSingleSchoolStudent,
+  allStaffStudent,
   singleSchoolStudentAnalytics,
   groupStudentAnalytics,
   allGroupSchoolStudent,
+  staffStudentAnalytics,
 } from '../../controller/student/studentController.js';
 
 const router = Router();
@@ -22,6 +25,26 @@ router.get(
   schoolAuthorization,
   pagination,
   catchAsync(allSingleSchoolStudent),
+);
+
+router.get(
+  '/:schoolId/staff/:staffId/all',
+  userAuthorization,
+  roleAuthorization([Role.SCHOOLSTAFF]),
+  schoolAuthorization,
+  staffAuthorization,
+  pagination,
+  catchAsync(allStaffStudent),
+);
+
+router.get(
+  '/:schoolId/staff/:staffId/analytics',
+  userAuthorization,
+  roleAuthorization([Role.SCHOOLSTAFF]),
+  schoolAuthorization,
+  staffAuthorization,
+  pagination,
+  catchAsync(staffStudentAnalytics),
 );
 
 router.get(
